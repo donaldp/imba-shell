35f54777989489d6339e009da1411130b5d3f2f1
Donald Pakkies <donaldpakkies@gmail.com>
2021-11-14 14:03:25 +0200

    feat(build): compile imba and ts

--- types/Command.d.ts
+++ types/Command.d.ts
@@ -1,11 +1,16 @@
+/// <reference types="node" />
 export var __esModule: boolean;
 export default Command;
 declare class Command {
     args: string[];
+    get isRuntime(): boolean;
     printVersion(): void;
-    invalidCommand(): void;
+    displayHelp(): void;
+    invalidCommand(): never;
     run(): any;
+    exec(): _$child_processφ.ChildProcess;
     handle(): any;
     [Ψ__init__]($$?: any): void;
 }
+import _$child_processφ = require("child_process");
 declare const Ψ__init__: unique symbol;
